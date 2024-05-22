d17792e2c606264947e1a2aec6277fc92e5cb7e2
Priyam-Pramanik <86661996+Priyam-Pramanik@users.noreply.github.com>
2024-05-22 11:30:09 +0200

    Add files via upload

---
+++ dart.js
@@ -0,0 +1,21 @@
+stat = "";
+
+function preload(){
+    loadImage('Dart Board.jpg');
+}
+function setup(){
+   canvas= createCanvas(350, 400);
+   canvas.center();
+   objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+   document.getElementById("status").innerHTML = "Detecting Objects.";
+}
+function modelLoaded(){
+    stat = true;
+    objectDetector.detect(img, gotResults);
+}
+function gotResults(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+}
