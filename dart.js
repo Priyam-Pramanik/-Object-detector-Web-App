791e48502699c1676c1ef0e389f2d91e45ab24a7
Priyam-Pramanik <86661996+Priyam-Pramanik@users.noreply.github.com>
2024-06-03 22:05:09 +0200

    Add files via upload

--- dart.js
+++ dart.js
@@ -1,10 +1,12 @@
+
 stat = "";
+objects = [];
 
 function preload(){
-    loadImage('Dart Board.jpg');
+   img = loadImage('Dart Board.jpg');
 }
 function setup(){
-   canvas= createCanvas(350, 400);
+   canvas= createCanvas(640, 420);
    canvas.center();
    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
    document.getElementById("status").innerHTML = "Detecting Objects.";
@@ -18,4 +20,19 @@ function gotResults(error, results){
         console.log(error);
     }
     console.log(results);
+    objects = results;
+}
+function draw(){
+    image(img, 0, 0, 640, 420);
+    if(stat != ""){
+        for(i = 0; i < objects.length; i++){
+            confidence = floor(objects[i].confidence* 100);
+            text(objects[i].label + " " + confidence + " %", objects[i].x, objects[i].y);
+            fill("#FF0000");
+            stroke("#FF0000");
+            noFill("");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height,);
+            document.getElementById("status").innerHTML = "There is 1 object in the image out which "+ objects.length+ " are detected."
+        }
+    }
 }
